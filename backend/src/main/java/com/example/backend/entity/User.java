b509c71d2f1a944b6765d0202b3819c8f34fb344
Mousigan Iyyamperumal <237917370+mousigan@users.noreply.github.com>
2026-03-16 15:00:43 +0530

    Add mobileNumber field and update User constructor
    Added mobileNumber field to User entity and updated constructor.

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -26,17 +26,20 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    private String mobileNumber;
+    
     private Integer creditPoints = 0;
 
     public enum Role {
-        CUSTOMER, PENDING_VENDOR, VENDOR, ADMIN
+        CUSTOMER, PENDING_VENDOR, VENDOR, ADMIN, ASTADMIN
     }
 
-    public User(String name, String email, String password, Role role) {
+    public User(String name, String email, String password, Role role, String mobileNumber) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.role = role;
+        this.mobileNumber = mobileNumber;
         this.creditPoints = 0;
     }
 }
